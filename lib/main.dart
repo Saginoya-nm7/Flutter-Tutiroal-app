455153267e8f4f7bd0c52d22bfc91fa4911b3ced
Saginoya-nm7 <sqrt.trill425@gmail.com>
2020-07-19 15:15:19 +0900

    [Init] Hello, Flutter!

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+            child: Text("Hello, Flutter!")
+        ),
+      ),
+    );
+  }
+}
